d70b6bedfded10c82fa27e7a82155eaf8ac1f2a4
guolindong <guolindong@gmail.com>
2018-08-23 20:38:33 +0800

    for B3

--- src/scala_pointcloud.cpp
+++ src/scala_pointcloud.cpp
@@ -1,18 +1,36 @@
-//
-//
-#include <ibeosdk/scala.hpp>
-#include <ibeosdk/IpHelper.hpp>
+// 2018-08-23 CyberC3@SJTU
+
+#define Pi (3.14159265)
+
 #include <cstdlib>
+#include <iostream>
+
+#include <boost/shared_ptr.hpp>
+
 #include <ros/ros.h>
 #include <sensor_msgs/PointCloud2.h>
 
-#include <ibeo_scala_node/Object.h>
-#include <ibeo_scala_node/ObjectArray.h>
-#include <ibeo_scala_node/Point2D.h>
+#include <ibeosdk/scala.hpp>
+#include <ibeosdk/CommandId.hpp>
+#include <ibeosdk/devices/IbeoDeviceBase.hpp>
+#include <ibeosdk/devices/IbeoScala.hpp>
+#include <ibeosdk/devices/IbeoDevice.hpp>
+#include <ibeosdk/devices/IbeoEthDevice.hpp>
+#include <ibeosdk/devices/IbeoEthType.hpp>
+#include <ibeosdk/IpHelper.hpp>
 
-#include <iostream>
+#include <ibeo_scala/Object.h>
+#include <ibeo_scala/ObjectArray.h>
+#include <ibeo_scala/Point2D.h>
 
+#include <pcl-1.7/pcl/point_cloud.h>
+#include <pcl-1.7/pcl/point_types.h>
+#include <pcl-1.7/pcl/PCLPointCloud2.h>
+#include <pcl-1.7/pcl/io/pcd_io.h>
+#include <pcl_conversions/pcl_conversions.h>
 
+#include <pcl-1.7/pcl/common/common_headers.h>
+#include <pcl-1.7/pcl/common/transforms.h>
 
 ibeosdk::IbeoSDK ibeoSDK;
 const ibeosdk::Version::MajorVersion majorVersion(5);
@@ -20,11 +38,12 @@ const ibeosdk::Version::MinorVersion minorVersion(2);
 const ibeosdk::Version::Revision revision(2);
 const ibeosdk::Version::PatchLevel patchLevel;
 const ibeosdk::Version::Build build;
-const std::string info = "IbeoSdkScalaLiveDemo";
+const std::string info = "IbeoSdkScalaNodeROS";
 
 ibeosdk::Version appVersion(majorVersion, minorVersion, revision, patchLevel, build, info);
 
 ibeosdk::TimeConversion tc;
+
 class AllScalaListener : public ibeosdk::DataListener<ibeosdk::FrameEndSeparator>,
                          public ibeosdk::DataListener<ibeosdk::Scan2208>,
                          public ibeosdk::DataListener<ibeosdk::ObjectListScala2271>,
@@ -32,17 +51,17 @@ class AllScalaListener : public ibeosdk::DataListener<ibeosdk::FrameEndSeparator
 
 public:
     ros::NodeHandle nh;
-    ros::Publisher pubScalaScan;
-    ros::Publisher pubScalaObject;
+    ros::Publisher pub_scala_points;
+    ros::Publisher pub_scala_objects;
 
-    ibeo_scala_node::Object object;
-    ibeo_scala_node::Point2D contour_points;
-    ibeo_scala_node::ObjectArray objectArray;
+    ibeo_scala::Object object;
+    ibeo_scala::Point2D contour_points;
+    ibeo_scala::ObjectArray object_array;
 
     AllScalaListener()
     {
-        pubScalaScan = nh.advertise<sensor_msgs::PointCloud2>("/scala_scan", 5);
-        pubScalaObject = nh.advertise<ibeo_scala_node::ObjectArray>("/scala_object",5);
+        pub_scala_points = nh.advertise<sensor_msgs::PointCloud2>("scala_points", 5);
+        pub_scala_objects = nh.advertise<ibeo_scala::ObjectArray>("scala_objects", 5);
     }
 
     virtual ~AllScalaListener(){}
@@ -56,116 +75,171 @@ public:
 
     void onData(const ibeosdk::Scan2208* const scanList)
     {
-        
         std::vector<ibeosdk::SubScan2208> subscan = scanList->getSubScans();
-        //std::vector <ibeosdk::ScanPoint2208> scan_points = scanList->get
-        // std::cout << "number scan: " << subscan.size() << std::endl;
         std::vector<ibeosdk::ScanPoint2208> scanpoint;
-        double dis,angle,x,y;
+        float dis,angle;
         int flags;
-        int sub_scan_size = subscan.size();
 
-        for(int i = 0; i < sub_scan_size; ++i)
+        pcl::PointCloud<pcl::PointXYZI> scala_cloud;
+
+        for(int i = 0; i < subscan.size(); ++i)
         {
             scanpoint = subscan[i].getScanPoints();
-            //std::cout << "pointsize: " << scanpoint.size() << std::endl;
             int scan_point_size = scanpoint.size();
-            for(int j = 0; j < scanpoint.size(); ++j)
+
+            scala_cloud.is_dense = false;
+            scala_cloud.width = scan_point_size;
+            scala_cloud.height = 1;
+            scala_cloud.resize(scala_cloud.height * scala_cloud.width);
+
+            for(int j = 0; j < scan_point_size; ++j)
             {
-                flags = scanpoint[j].getFlags();
-                if(true)
-                //if(!flags & (ibeosdk::ScanPoint2208::SPFML_Ground | ibeosdk::ScanPoint2208::SPFML_Dirt | ibeosdk::ScanPoint2208::SPFML_Rain))
+//                pcl::PointXYZI point;
+                ibeosdk::ScanPoint2208 scala_point = scanpoint[j];
+                flags = scala_point.getFlags();
+                if(!(flags/* & (ibeosdk::ScanPoint2208::SPFML_Ground | ibeosdk::ScanPoint2208::SPFML_Dirt | ibeosdk::ScanPoint2208::SPFML_Rain)*/))
+                //if(!(flags & (ibeosdk::ScanPoint2208::SPFML_Ground | ibeosdk::ScanPoint2208::SPFML_Dirt | ibeosdk::ScanPoint2208::SPFML_Rain)))
                 {
-                    dis = scanpoint[j].getRadialDistance()/100.0;   // 米
-                    angle = 90 - (scanpoint[j].getHorizontalAngle()/32.0);  //
+                    dis = scala_point.getRadialDistance()/100.0;   // 米
+                    angle = -90 + (scala_point.getHorizontalAngle()/32.0);  //
+                    //layer to phi
+                    int layer = scala_point.getLayerId();//0123
+                    double phi = layer * 0.8 - 1.2;
+                    scala_cloud.points[j].intensity = scala_point.getEchoPulseWidth();
+                    scala_cloud.points[j].z = dis * (sin(DEG2RAD(phi)));
+                    scala_cloud.points[j].x = dis * (cos(DEG2RAD(phi))) * (cos(DEG2RAD(angle)));
+                    scala_cloud.points[j].y = dis * (cos(DEG2RAD(phi))) * (sin(DEG2RAD(angle)));
                 }
-
             }
-
         }
 
-//        sensor_msgs::PointCloud2 pointcloud;
-//        pointcloud.header.frame_id = "scala";
-//        pointcloud.header.stamp = ros::Time::now();
-//        pubScalaScan.publish(pointcloud);
+        sensor_msgs::PointCloud2 cloud_to_pub;
+        pcl::toROSMsg(scala_cloud, cloud_to_pub);//pcl::toROSMsg() should be done before setting frame_id
+        cloud_to_pub.header.frame_id = "scala";
+        cloud_to_pub.header.stamp = ros::Time::now();
 
+        pub_scala_points.publish(cloud_to_pub);
     }
 
     void onData(const ibeosdk::ObjectListScala2271* const objectList)
     {
         std::vector<ibeosdk::ObjectScala2271> object_points = objectList->getObjects();
-        objectArray.objects.clear();
+        object_array.objects.clear();
         std::cout << "object num: " << object_points.size() << std::endl;
 
         for(int i =0; i<object_points.size();++i)
         {
-            object.id = object_points.at(i).getObjectId();
-            object.age = object_points.at(i).getFilteredObjectAttributes().getObjectAge();
-            object.classification = object_points.at(i).getFilteredObjectAttributes().getClassification();
-            object.velocity.x = object_points.at(i).getFilteredObjectAttributes().getRelativeVelocitySigma().getX()/100.0;
-            object.velocity.y = object_points.at(i).getFilteredObjectAttributes().getRelativeVelocitySigma().getX()/100.0;
-            object.object_closest.x = object_points.at(i).getFilteredObjectAttributes().getPositionClosestObjectPoint().getX()/100.0;
-            object.object_closest.y = object_points.at(i).getFilteredObjectAttributes().getPositionClosestObjectPoint().getY()/100.0;
-            object.object_box_size.x = object_points.at(i).getFilteredObjectAttributes().getObjectBoxSizeSigma().getX()/100.0;
-            object.object_box_size.y = object_points.at(i).getFilteredObjectAttributes().getObjectBoxSizeSigma().getY()/100.0;
+            object.id = object_points[i].getObjectId();
+            object.age = object_points[i].getFilteredObjectAttributes().getObjectAge();
+            object.classification = object_points[i].getFilteredObjectAttributes().getClassification();
+            object.velocity.x = object_points[i].getFilteredObjectAttributes().getRelativeVelocitySigma().getX()/100.0;
+            object.velocity.y = object_points[i].getFilteredObjectAttributes().getRelativeVelocitySigma().getX()/100.0;
+            object.object_closest.x = object_points[i].getFilteredObjectAttributes().getPositionClosestObjectPoint().getX()/100.0;
+            object.object_closest.y = object_points[i].getFilteredObjectAttributes().getPositionClosestObjectPoint().getY()/100.0;
+            object.object_box_size.x = object_points[i].getFilteredObjectAttributes().getObjectBoxSizeSigma().getX()/100.0;
+            object.object_box_size.y = object_points[i].getFilteredObjectAttributes().getObjectBoxSizeSigma().getY()/100.0;
 
-            for(int j = 0; j < object_points.at(i).getFilteredObjectAttributes().getContourPoints().size();++j)
+            for(int j = 0; j < object_points[i].getFilteredObjectAttributes().getContourPoints().size();++j)
             {
-                contour_points.x = object_points.at(i).getFilteredObjectAttributes().getContourPoints().at(j).getXSigma()/100.0;
-                contour_points.y = object_points.at(i).getFilteredObjectAttributes().getContourPoints().at(j).getYSigma()/100.0;
-
+                contour_points.x = object_points[i].getFilteredObjectAttributes().getContourPoints().at(j).getXSigma()/100.0;
+                contour_points.y = object_points[i].getFilteredObjectAttributes().getContourPoints().at(j).getYSigma()/100.0;
                 object.contour.push_back(contour_points);
             }
-            objectArray.objects.push_back(object);
+            object_array.objects.push_back(object);
         }
-        objectArray.header.frame_id = "";
-        objectArray.header.stamp = ros::Time::now();
-        pubScalaObject.publish(objectArray);
+        object_array.header.frame_id = "scala";
+        object_array.header.stamp = ros::Time::now();
+        pub_scala_objects.publish(object_array);
     }
 
     void onData(const ibeosdk::DeviceStatus6303* const status)
     {
-       // ibeosdk::logInfo << std::setw(5) << status->getSerializedSize() << " Bytes "
-       //                                  << "DevStat 0x6306 received" << std::endl;
-
+        // ibeosdk::logInfo << std::setw(5) << status->getSerializedSize() << " Bytes "
+        //                                  << "DevStat 0x6306 received" << std::endl;
     }
 };
 
 
 int main(int argc, char **argv)
 {
-    ros::init(argc, argv, "scala_pointcloud");
-
+    ros::init(argc, argv, "scala_node");
     ros::NodeHandle pnh("~");
-    // ip param
-    std::string device_ip;
-    int port;
-    pnh.param<std::string>("device_ip",device_ip,"192.168.1.52");
-    pnh.param<int>("port", port, 12004);
+
+    int scala_type;
+    std::string device_destination_ip;//for scala B3
+    std::string device_ip;//for scala B2
+    std::string port;
+    std::string log_level;
+    std::string multicast_interface_ip;
+    bool use_multicast;
+
+    pnh.param<int>("scala_type", scala_type, 3);
+    pnh.param<std::string>("device_destination_ip", device_destination_ip, "224.100.100.100");
+    pnh.param<std::string>("device_ip", device_ip, "192.168.1.53");
+    pnh.param<std::string>("port", port, "22017");
+    pnh.param<std::string>("multicast_interface_ip", multicast_interface_ip, "224.0.0.1");
+    pnh.param<std::string>("log_level", log_level, "Quiet");
+    pnh.param<bool>("use_multicast", use_multicast, true);
+
     std::cerr << argv[0] << " Version " << appVersion.toString();
     std::cerr << " using IbeoSDK " << ibeoSDK.getVersion().toString() << std::endl;
 
+    //log file and log_level
     const off_t magLogFileSize = 1000000;
     ibeosdk::LogFileManager logFileManager;
     ibeosdk::LogFile::setTargetFileSize(magLogFileSize);
-    const ibeosdk::LogLevel ll = ibeosdk::logLevelFromString("Release");
+    const ibeosdk::LogLevel ll = ibeosdk::logLevelFromString(log_level);
     ibeosdk::LogFile::setLogLevel(ll);
     logFileManager.start();
 
-    AllScalaListener allScalaListener;
-    ibeosdk::IbeoScala scala(device_ip, port, ibeosdk::IbeoTypeEthTcp()); //B2
-    //ibeosdk::IbeoScala scala(device_ip, port, ibeosdk::IbeoTypeEthUdp()); //B3
-    scala.setLogFileManager(&logFileManager);
-    scala.registerListener(&allScalaListener);
+    //network address configuration
+    std::stringstream ss;
+    ss.str(port);
+    uint16_t portVal;
+    ss >> portVal;
+    const boost::asio::ip::address_v4 interface_ip; //gateway
+    interface_ip.from_string(multicast_interface_ip);
 
-    scala.getConnected();
+    boost::shared_ptr<ibeosdk::IbeoScala> scala;
+
+    switch (scala_type)
+    {
+        //using scalaB2
+        case 2:
+            scala.reset(new ibeosdk::IbeoScala(device_ip,
+                                               portVal,
+                                               ibeosdk::IbeoTypeEthTcp()));
+            break;
+        //using scala B3
+        case 3:
+            if (use_multicast) {
+                scala.reset(new ibeosdk::IbeoScala(device_destination_ip,
+                                                   portVal,
+                                                   ibeosdk::IbeoTypeEthUdpMulticast(),
+                                                   interface_ip));
+            } else {
+                scala.reset(new ibeosdk::IbeoScala(device_destination_ip,
+                                                   portVal,
+                                                   ibeosdk::IbeoTypeEthUdp(),
+                                                   interface_ip));
+            }
+            break;
+        default:
+            printf("should set correct scala_type in launch file.\n");
+            break;
+    }
+
+    AllScalaListener allScalaListener;
+    scala->setLogFileManager(&logFileManager);
+    scala->registerListener(&allScalaListener);
+    scala->getConnected();
+
     ros::Rate loop_rate(25);     //25Hz
     while(ros::ok())
     {
-        if(!scala.isConnected())
+        if(!scala->isConnected())
         {
-            std::cout << "Failed!\n";
-            ROS_INFO("!scala.isConnected\n");
+            printf("!scala.isConnected\n");
             return -1;
         }
         ros::spinOnce();
@@ -173,6 +247,8 @@ int main(int argc, char **argv)
     }
     return 0;
 }
+
+
 
 
 
